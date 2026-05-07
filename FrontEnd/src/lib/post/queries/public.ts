343b11145fab8334d4bbb6c1d305749b67adb1e3
Luiz Barcelar <luizbarcelar2014@gmail.com>
2026-05-07 01:03:17 -0300

    refactor: remove sqlite from frontend

--- FrontEnd/src/lib/post/queries/public.ts
+++ FrontEnd/src/lib/post/queries/public.ts
@@ -1,56 +1,39 @@
-import { PostModelFromApi } from '@/models/post/post-model';
-import { postRepository } from '@/repositories/post';
+import { PostModelFromApi } from '@/models/post/post-model'
 import { apiRequest } from '@/utils/api-request';
 import { unstable_cache } from 'next/cache';
-import { notFound } from 'next/navigation';
-import { cache } from 'react';
+import { cache } from 'react'
 
 export const findAllPublicPostsCached = cache(
   unstable_cache(
     async () => {
-      return await postRepository.findAllPublic();
+      return apiRequest<PostModelFromApi[]>(`/post`, {
+        next: {
+          tags: ['posts'],
+          revalidate: 86400,
+        },
+      });
     },
     ['posts'],
     {
       tags: ['posts'],
     },
   ),
-);
-
-export const findAllPublicPostsFromApiCached = cache(async () => {
-  const postsResponse = await apiRequest<PostModelFromApi[]>(`/post`, {
-    next: {
-      tags: ['posts'],
-      revalidate: 86400,
-    },
-  });
-
-  return postsResponse;
-});
+)
 
 export const findPublicPostBySlugCached = cache((slug: string) => {
   return unstable_cache(
-    async (slug: string) => {
-      const post = await postRepository
-        .findBySlugPublic(slug)
-        .catch(() => undefined);
-
-      if (!post) notFound();
-
-      return post;
+    async () => {
+      return apiRequest<PostModelFromApi>(`/post/${slug}`, {
+        next: {
+          tags: [`post-${slug}`],
+          revalidate: 86400,
+        },
+      });
     },
     [`post-${slug}`],
-    { tags: [`post-${slug}`] },
-  )(slug);
-});
-
-export const findPublicPostBySlugFromApiCached = cache(async (slug: string) => {
-  const postsResponse = await apiRequest<PostModelFromApi>(`/post/${slug}`, {
-    next: {
+    {
       tags: [`post-${slug}`],
-      revalidate: 86400,
-    },
-  });
-
-  return postsResponse;
+    }
+  )();
 });
+
